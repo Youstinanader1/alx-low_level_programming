d0c514accb1e47ee7c38f44a5c20c566a38cb920
Youstina <youstina.nader8888@gmail.com>
2023-04-04 10:38:47 -0700

    m

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,14 @@
 # !/bin/bash
 #include <stdio.h>
- /** 
-  * main - A program that prints a line with puts function
-  * Return: 0 (success)
-  */
+/** 
+  * main - Entry point
+  *
+  * Description: A C  program that prints a line with puts function
+  *
+  * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts(""\*Programing is like building a multilingual puzzle");
+	puts("\"Programing is like building a multilingual puzzle");
 	 return (0);
 }
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/**
+ *main - Entry point
+ *
+ *Description:main-Prints out a string to stdout.
+ * 
+ *Return:Always0(Success)
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
